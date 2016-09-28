2ecd9a3b2cd127f258bc1633ec5bf8a41ff57ebe
ml7757 <matthew.w.laporte@gmail.com>
2016-09-28 16:26:25 +0200

    create form partial and new and create methods

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -3,4 +3,22 @@ class MentorsController < ApplicationController
   def index
     @mentors = Mentor.all
   end
+
+  def new
+    @mentor = Mentor.new
+  end
+
+  def create
+    @mentor = Mentor.new(mentor_params)
+    @mentor.save
+    redirect_to mentors_path
+  end
+
+  private
+
+  def mentor_params
+    params.require(:mentor).permit(:name, :start_time, :end_time,
+                                    :availability, :calendly_url,
+                                    :avatar, :competencies)
+  end
 end
