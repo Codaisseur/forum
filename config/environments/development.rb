d039ffef7ea1b7165ee7e53cd9b84d398f986ef2
stofstik <verstegen.daan@gmail.com>
2016-10-24 14:41:00 +0200

    formatting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,12 +32,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:             'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name: ENV["EMAIL"],
-    password: ENV["EMAIL_PWD"],
-    authentication: 'plain',
+    domain:              'gmail.com',
+    user_name:            ENV["EMAIL"],
+    password:             ENV["EMAIL_PWD"],
+    authentication:      'plain',
     enable_starttls_auto: true
   }
 
