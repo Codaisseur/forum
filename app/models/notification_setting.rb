52940ccc58fb516e72fb0e339a7958780cfc570b
MalenkavanOmmen <malenkavanommen@gmail.com>
2016-10-25 16:38:48 +0200

    notification preferences

--- app/models/notification_setting.rb
+++
@@ -1,4 +0,0 @@
-class NotificationSetting < ApplicationRecord
-  belongs_to :user
-  belongs_to :profile
-end
--- spec/factories/notification_settings.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :notification_setting do
-    user nil
-    asker_sees_replies false
-    member_sees_replies false
-  end
-end
