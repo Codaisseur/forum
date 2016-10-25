38c8962e6da39c4c1f3322d5ef51736b4d5adc2f
stofstik <verstegen.daan@gmail.com>
2016-10-25 17:17:35 +0200

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,6 @@ if Rails.env.production?
   traineeship4 = Course.create!( name: "Traineeship 4", starting_at: Date.new(2016,8), ending_at: Date.new(2016,9) )
   traineeship5 = Course.create!( name: "Traineeship 5", starting_at: Date.new(2016,10), ending_at: Date.new(2016,11) )
 else
-  NotificationSetting.delete_all
   Answer.delete_all
   Question.delete_all
   Profile.delete_all
@@ -138,9 +137,6 @@ else
     website: "https://www.developmentbootcamp.nl/t",
     user: admin
   )
-
-  notification_setting1 = NotificationSetting.create(asker_sees_replies: true, member_sees_replies: true, user: miriam, profile: pr_miriam)
-
 
   question1 = Question.create!( title: "How to implement Devise into your ruby app?", body: "Google. Google and more google and more google  and more google and more google and more google and more google and more google and more google and more google and more google and more google and more google", user: matthijs, topic: ruby)
   question2 = Question.create!( title: "How to implement Devise into your rails app?", body: "Google. Google and more google and more google  and more google and more google and more google and more google and more google and more google and more google and more google and more google and more google", user: matthijs, topic: rails )
