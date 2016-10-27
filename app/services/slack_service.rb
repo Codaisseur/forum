fbcc81b980ade8b15056c70b5fd12125ca8d4d5b
stofstik <verstegen.daan@gmail.com>
2016-10-27 11:58:22 +0200

    Fix forum url

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -3,11 +3,12 @@ require 'slack-notifier'
 class SlackService
   include Rails.application.routes.url_helpers
 
-  def initialize
+  def initialize(hostname)
     webhook  = ENV["SLACK_WEBHOOK"]
     channel  = '#testing_notifications'
     username = 'Forum Bot'
     @slack_notifier = Slack::Notifier.new webhook, channel: channel, username: username
+    @hostname = hostname
   end
 
   def slack_notification_new question
@@ -15,7 +16,7 @@ class SlackService
       "<!channel> #{question.user.profile.first_name} posted a new question in the Forum!\r\n
       #{question.title}\r\n
       Check out the question to see if you can help! :heart:\r\n
-      #{question_path(question)}"
+      #{question_url(only_path: false, host: @hostname, id: question.id)}"
     )
   end
 
@@ -23,7 +24,7 @@ class SlackService
     @slack_notifier.ping(
       "<!channel> #{question.members.last.profile.first_name} commented on #{question.title} in the Forum!\r\n
       Check out the question to see if you can help! :heart:\r\n
-      #{question_path(question)}"
+      #{question_url(only_path: false, host: @hostname, id: question.id)}"
     )
   end
 end
